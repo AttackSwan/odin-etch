17c933ddf1ecc18a0361fe4765d941562e4416dd
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-03-06 10:55:13 +1100

    Add color change on mouseover

--- javascript.js
+++ javascript.js
@@ -1,22 +1,24 @@
-let a = 2;
-let b = 2;
+let gridSize    = 12;
+const grid      = document.getElementById("grid");
 
-const container = document.getElementById("grid");
-console.log(container);
-
-function makeGrid(rows, cols){
+function makeGrid(size){
     //set number of rows and columns in CSS
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    console.log(container.style.getPropertyValue("--grid-rows"));
-    //create cells
-    for (i = 0; i < (rows * cols); i++){
-        //create a div
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    //create and append cells
+    for (i = 0; i < (size * size); i++){
         let cell = document.createElement('div');
-        cell.textContent = i+1;
-        //add cell to grid
-        container.appendChild(cell).className = "grid-item";
+        cell.classList.add("grid-item");
+        //add listener
+        cell.addEventListener('mouseover', (e) => {
+            colorCell(e);
+        });
+        grid.appendChild(cell);
     }
 }
 
-makeGrid(a, b);
+function colorCell(e){
+    e.target.style.backgroundColor = "pink";
+}
+
+makeGrid(gridSize);
