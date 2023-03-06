29489a1a161b5eb35fd8ba7df749d87627bf2b27
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-03-06 12:46:00 +1100

    Add button to clear grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-let gridSize    = 12;
-const grid      = document.getElementById("grid");
+const defaultGridSize   = 12;
+const grid              = document.getElementById("grid");
+const sizeButton        = document.getElementById("sizeBTN");
+const clearButton       = document.getElementById("clearBTN");
+const rainbowButton     = document.getElementById("rainbowBTN");
+const bgColor           = getComputedStyle(document.documentElement)
+                            .getPropertyValue('--bgColor');
+
+sizeButton.onclick      = () => getNewSize();
+clearButton.onclick     = () => clearCells();
+rainbowButton.onclick   = () => RGBCells();
+
+
+
+makeGrid(defaultGridSize);
 
 function makeGrid(size){
-    //set number of rows and columns in CSS
+    //set row and column layout
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     //create and append cells
@@ -17,8 +30,31 @@ function makeGrid(size){
     }
 }
 
+function getNewSize(){
+    let newSize = prompt("Please enter a new grid size between 1 and 256");
+    sizeValidate(newSize);
+}
+
+function sizeValidate(size){
+    while (size <= 0 || size > 256){
+        size = prompt("Invalid size! Please enter a new grid size between 1 and 256");
+    }
+    clearCells();    
+    makeGrid(size);
+}
+
+function clearCells(){
+    const cells = document.querySelectorAll('.grid-item');
+    cells.forEach((cell) => {
+        const element = cell;
+        element.style.backgroundColor = bgColor;
+    });
+}
+
+function RGBCells(){
+    alert("RGB Cells");
+}
 function colorCell(e){
     e.target.style.backgroundColor = "pink";
 }
 
-makeGrid(gridSize);
